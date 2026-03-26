0e6bde5160d0831145a9a8a4b1782dfc4e7a9d76
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2026-03-26 23:38:56 +0530

    components: implement checkbox

---
+++ src/lib/components/checkbox.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { Checkbox, type WithoutChildrenOrChild } from "bits-ui";
+	import clsx from "clsx";
+	import CheckIcon from "phosphor-svelte/lib/CheckIcon";
+
+	let { class: className, ...restProps }: WithoutChildrenOrChild<Checkbox.RootProps> = $props();
+</script>
+
+<Checkbox.Root
+	class={clsx(
+		"group inline-flex size-5 shrink-0 border-2 transition-all duration-75 data-[state=checked]:bg-emerald-400",
+		className
+	)}
+	{...restProps}
+>
+	<CheckIcon
+		class="size-4 h-full w-full transition-all duration-150 group-data-[state=checked]:opacity-100 group-data-[state=unchecked]:opacity-0"
+		weight="bold"
+	/>
+</Checkbox.Root>
