743a6f66e8b1cf870a207d837db004d9be7c0c57
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2026-03-26 16:31:43 +0530

    fix: attendance cutoff limit

--- src/routes/(auth)/settings/settings-schema.ts
+++ src/routes/(auth)/settings/settings-schema.ts
@@ -2,8 +2,8 @@ import z from "zod";
 
 export const settingsSchema = z
 	.object({
-		attendanceMinCutoff: z.int().min(0).max(100), // todo: fix
-		attendanceMaxCutoff: z.int().min(0).max(100),
+		attendanceMinCutoff: z.int().min(0).max(99),
+		attendanceMaxCutoff: z.int().min(0).max(99),
 		expandAttendanceSubjects: z.enum(["none", "critical", "all"]),
 		invalidAttendanceMarker: z.enum(["double-hyphen", "ndash", "mdash", "single-hyphen"]),
 		showAttendanceBarByDefault: z.boolean()
